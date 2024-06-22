3835d0faa0f0995011b23252c9ac856faefab2e7
emmanesgana <emman.esgana@gmail.com>
2024-06-22 10:32:15 +0800

    Update Main.js

--- components/Main.js
+++ components/Main.js
@@ -14,7 +14,7 @@ export default function Main(props) {
             <div className="container-info">
                 <div className="container-location">
                     <p className="content-location"><FontAwesomeIcon icon={faLocationDot} style={{ color: "#F55A5A", }} /> {props.location.toUpperCase()}</p>
-                    <a className="content-googlemaplink" href={props.googleMapsUrl}>View on Google Maps</a>
+                    <a className="content-googlemaplink" href={props.googleMapsUrl} target="_blank">View on Google Maps</a>
                 </div>
                 <h1 className="content-title">{props.title}</h1>
                 <p className="content-date">{`${props.startDate} - ${props.endDate}`}</p>
